045466886fd81e12ce644b0f04b1715a07d0c9d0
Bruno Lima <bruno@threeo.com.br>
2020-04-30 16:15:23 -0300

    Alterações no dockerfile e environment.prod.ts para eatendimento

--- emad-web/src/environments/environment.prod.ts
+++ emad-web/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://saude.icconsulting.com.br/api',
+  apiUrl: 'http://saude.icconsulting.com.br/api-eatende',
   superAdmin: 3,
   avancado : 2,
   admin: 1, 
-  socketUrl:'http://saude.icconsulting.com.br/api',
+  socketUrl:'http://saude.icconsulting.com.br/api-eatende',
   utc : 3
 };
