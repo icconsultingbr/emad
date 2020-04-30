a655ea0d97380f0e3017f58341f598ac4f6c8f90
Wagner Kenji Yatabe <30706676+wyatabe@users.noreply.github.com>
2020-04-30 11:32:38 -0300

    Ajustes do emad para funcionar com redirect do IIS.

--- emad-web/src/environments/environment.prod.ts
+++ emad-web/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://emad.icconsulting.com.br/api',
+  apiUrl: 'http://saude.icconsulting.com.br/api',
   superAdmin: 3,
   avancado : 2,
   admin: 1, 
-  socketUrl:'https://emad.icconsulting.com.br/api',
+  socketUrl:'http://saude.icconsulting.com.br/api',
   utc : 3
 };
