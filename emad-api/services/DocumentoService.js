e7644c00bfbc53251229b23a2d84650b5abd7394
darioferreirafranca <89949845+darioferreirafranca@users.noreply.github.com>
2021-09-13 19:31:31 -0300

    Criacao de rotina de upload de arquivos. (#35)

--- emad-api/services/DocumentoService.js
+++ emad-api/services/DocumentoService.js
@@ -2,27 +2,65 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const path = require('path')
 var config = require('config');
+const { stringify } = require("querystring");
 
 function DocumentoService() { }
 
 DocumentoService.prototype.uploadImage = async function (file) {
     return new Promise((resolve, reject) => {
-        try{
+        try {
             if (!fs.existsSync(config.folderUpload)) {
                 fs.mkdirSync(config.folderUpload);
             }
             var id = file.id.value;
             if (!id)
                 id = uuidv4();
-    
+
             let base64Image = file.base64.split(";base64,").pop();
-    
+
             fs.writeFile(`${config.folderUpload}/${id}.${file.extension}`, base64Image, { encoding: "base64" }, function () {
-            resolve(id + `.${file.extension}`);
+                resolve(id + `.${file.extension}`);
             }, function (error) {
                 reject('Erro ao criar arquivo:: ' + error)
             });
-        } catch(e){
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+DocumentoService.prototype.uploadImageList = async function (files) {
+    return new Promise((resolve, reject) => {
+        try {
+
+            var listAllImage = [];
+
+            if (!fs.existsSync(config.folderUploadExames)) {
+                fs.mkdirSync(config.folderUploadExames);
+            }
+
+            for (const itemfile of files) {
+                var id = itemfile.id.value;
+                if (!id)
+                    id = uuidv4();
+
+                let base64Image = itemfile.base64.split(";base64,").pop();
+
+                var imagePro = {
+                    nome: itemfile.name,
+                    caminho: `${config.folderUploadExames}/${id}.${itemfile.extension}`,
+                    tipo: `${itemfile.extension}`,
+                };
+
+                listAllImage.push(imagePro)
+
+                fs.writeFile(`${config.folderUploadExames}/${id}.${itemfile.extension}`, base64Image, { encoding: "base64" }, function () {
+                    resolve(listAllImage);
+                }, function (error) {
+                    reject('Erro ao criar arquivo:: ' + error)
+                });
+            }
+        } catch (e) {
             reject(e);
         }
     });
--- emad-web/src/app/_core/_models/Exame.ts
+++ emad-web/src/app/_core/_models/Exame.ts
@@ -1,18 +1,20 @@
 import { Input } from "@angular/core";
+import { Guid } from "guid-typescript";
 import { ItemExame } from "./ItemExame";
 
 export class Exame {
     id: Number;
     @Input() idEstabelecimento: number = +JSON.parse(localStorage.getItem("est"))[0].id;
+    @Input() nomeProfissional: string = JSON.parse(localStorage.getItem("currentUser"));
     @Input() idPaciente: number;
-    @Input() nomePaciente: string;    
+    @Input() nomePaciente: string;
     @Input() situacao: string;
-    @Input() idTipoExame: number;    
-    @Input() itensExame: ItemExame[] = [];  
+    @Input() idTipoExame: number;
+    @Input() itensExame: ItemExame[] = [];
     @Input() acao: string;
-    @Input() mensagemPaciente: string;    
+    @Input() mensagemPaciente: string;
     @Input() ano: number;
-    @Input() mes: string;	
+    @Input() mes: string;
     @Input() numero: string;
     @Input() resultadoFinal: string;
 }
