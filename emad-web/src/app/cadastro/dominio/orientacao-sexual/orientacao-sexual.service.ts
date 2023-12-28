1393548740459f22eb5f01ce55f4a32888f144c1
Dener Sousa <143218088+denersousa-threeo@users.noreply.github.com>
2023-12-28 18:37:54 -0300

    Criação do CRUD, orientação sexual. (#94)
    * FEAT: Calendario
    
    * 1 parcial da agenda
    
    * Restaurando stash com as alterações
    
    * Foram criados nos métodos para consulta de profissional pelo estabelecimento e pela especialidade.
    
    * Condição para trazer apenas os profissionais que tem agenda para a data informada
    
    * correção no fechamento de modal
    
    * Verificando duplicidade de agendamento paciente
    
    * Formulario dinamico
    
    * Implementação de consulta por equipe e correção na consulta de dados.
    
    adionado cor diferenciada para cada agendamento.
    
    * Foi adicionados métodos de consulta para exbição de profissionais e equipe de acordo com os agendamento, para que não aja conflito de agenda nos dias de atendimento do profissional e equipe.
    
    * Foram alterados as funções de consulta de profissional e equipe;
    Correção no fechamento da modal;
    Correção ao limpar campos de formulário;
    
    * Correção de bug: Ao selecionar o tipo de atendimento e depois troca deixa com um valor inválido.
    
    * Correção: Ao selecionar uma data menor que a atua, mostramos um alerta
    
    * Correção: Quando não ouver profissional/equipe para atendimento na data informada, mostramos um alerta na tela
    
    * Correção: Apresentação de data e hora juntas na visuzação do agendamento
    
    * MELHORIA: Criação CRUD de orientação sexual.
    
    ---------
    
    Co-authored-by: Bruno Renan de Lima <brunorenn@gmail.com>

---
+++ emad-web/src/app/cadastro/dominio/orientacao-sexual/orientacao-sexual.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { GenericsService } from '../../../_core/_services/generics.service';
+import { Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class OrientacaoSexualService extends GenericsService {
+
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  public fields: any[] = [
+    {
+      field: 'id',
+      type: 'hidden',
+      label: 'Id',
+      grid: true,
+      form: true,
+      required: false,
+      validator: ['', '']
+    },
+    {
+      field: 'nome',
+      type: 'text',
+      label: 'Nome',
+      grid: true,
+      form: true,
+      required: true,
+      validator: ['', Validators.required]
+    },
+    {
+      field: 'situacao',
+      type: 'checkbox',
+      label: 'Situação',
+      grid: true,
+      form: true,
+      translate: { 1: 'Ativo', 0: 'Inativo' },
+      required: true,
+      validator: ['', Validators.required]
+    }
+  ];
+}
